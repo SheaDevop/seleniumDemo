1ebc9f7442a773973564d56191a4a5bfafb98165
SheaDevOp <nacho.dieguez18@gmail.com>
2023-06-01 19:13:43 -0300

    assertion with node

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,5 +1,7 @@
 //bring selenium module
 const { Builder, By, Key } = require("selenium-webdriver");
+//bring assertion node built in pkg
+const assert = require("assert");
 
 //define the testing function
 async function example() {
@@ -9,7 +11,7 @@ async function example() {
 
 
   //navigate to the application
-  await driver.get("/application/EndPoint");
+  await driver.get("http://application/EndPoint");
 
 
   //test something in the app
@@ -18,6 +20,20 @@ async function example() {
         //(write some text in and press enter in this case)
   await driver.findElement(By.id("elementToSelect")).sendKeys("Learn Selenium", Key.RETURN);
 
+  //assert (checking that our test do what it has to do)
+    //find the element by xpath
+      //(should be the last element in the path cause we just added it)
+    //get the text from the selected field
+      //then return it so it gets stored in the variable
+  let fieldTextValue = await driver.findElement(By.xpath("//li[last()]")).getText()
+    .then(function(value){
+      return value
+    });
+
+  //checks that the value of the var is correct
+  assert.strictEqual(fieldTextValue, "Learn Selenium");
+
+  //if it doesn't behave how it supposed to we'll get an assertion error
 
 
   //close the browser
