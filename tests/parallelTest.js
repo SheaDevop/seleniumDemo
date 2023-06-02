959bf20b8a6822ef7f353bb3a068f3e29ea8dbb1
SheaDevOp <nacho.dieguez18@gmail.com>
2023-06-02 16:16:41 -0300

    parallelization with mocha

---
+++ tests/parallelTest.js
@@ -0,0 +1,57 @@
+//bring selenium module
+const { Builder, By, Key } = require("selenium-webdriver");
+//bring mocha
+const { describe, it } = require("mocha");
+//require chai pkg
+  //should style 
+var should = require('chai').should();
+
+//describe block
+describe("add some more text", function(){
+  
+  //it block
+  it("successfully adds more text to the text field and submit", async function(){
+
+    //launch the browser
+    let driver = await new Builder().forBrowser("firefox").build();
+    
+    
+    //navigate to the application
+    await driver.get("/application/EndPoint");
+    
+    
+    //test something in the app
+      //select the element to interact with
+        //do somthing with the element
+          //(write some text in and press enter in this case)
+    await driver.findElement(By.id("elementToSelect")).sendKeys("Learn Selenium parallelization", Key.RETURN);
+    
+    
+    //assert (checking that our test do what it has to do)
+      //find the element by xpath
+        //(should be the last element in the path cause we just added it)
+      //get the text from the selected field
+        //then return it so it gets stored in the variable
+    let fieldTextValue = await driver.findElement(By.xpath("//li[last()]")).getText()
+    .then(function(value){
+      return value
+    });
+    
+
+    //assert using chai should
+    fieldTextValue.should.equal("Learn Selenium parallelization")
+
+    
+    //close the browser
+    await driver.quit();
+  });
+});
+
+
+//to run the test run ==> npx mocha --no-timeouts 'tests/*.js'
+  //(no timeouts cause we're using async code)
+
+//to make this simpler allocate tests in "test" dir and run ==> npx mocha --no-timeouts
+  //or better replace test script in pkg.json for "mocha --no-timeouts 'tests/*.js'"
+
+//to run the tests in parallel add the flag  --parallel to the test script in pkg.json
